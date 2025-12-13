90a680134ef9a87d410ca0a621765e00e8c300df
PruthvirajGire18 <girepruthviraj@gmail.com>
2025-12-14 00:09:10 +0530

    get all blogs error fixed

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -94,3 +94,12 @@ export const deleteBlog=async(req,res)=>{
         return res.status(500).json({message:"Internal server error"});
     }
 }
+export const getAllBlogsOfAllUsers=async(req,res)=>{
+    try {
+        const blogs=await Blog.find().populate("user","name email").sort({createdAt:-1});
+        return res.status(200).json({blogs});
+    } catch (error) {
+        console.error("Error fetching blogs of all users:", error);
+        return res.status(500).json({message:"Internal server error"});
+    }
+}
